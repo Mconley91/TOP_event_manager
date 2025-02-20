db6adbf489906ee04dc90e8e4b6cda839d587e46
mconley91 <matthew.e.conley@gmail.com>
2025-02-20 17:43:46 -0500

    add get_most_common_regtimes which returns top 3 most common registration hours

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -27,13 +27,29 @@ def get_registration_time(regdate)
     hours_12 = hours_24
   end
   minutes = regdate.split(" ")[1].split(':')[1]
-  regtime = "#{hours_12}:#{minutes} #{am_or_pm}"
+  "#{hours_12}:#{minutes} #{am_or_pm}"
+end
+
+def get_most_common_regtimes(regtimes)
+  hours_to_eval = []
+  regtimes.each do |time|
+    time_split = time.split(/\W+/)
+    hour = time_split[2] == 'PM' ? time_split[0].to_i + 12 : time_split[0]
+    hours_to_eval << hour.to_i
+  end
+  top_3_hours_array = hours_to_eval.tally.to_a.sort {|a,b| a[1] <=> b[1]}.reverse[0..2].map {|hour| 
+  if hour[0].to_i > 12
+    "#{hour[0].to_i - 12} PM"
+  else
+    "#{hour[0].to_i} AM"
+  end
+  }
+  top_3_hours_array
 end
 
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = File.read('secretkey.txt').strip
-
   begin
     legislators = civic_info.representative_info_by_address(
       address: zip,
@@ -48,17 +64,21 @@ end
 def write_letters(contents)
   template_letter = File.read('form_letter.erb')
   erb_template = ERB.new template_letter
+  regtimes = []
   contents.each do |row|
     id = row[0]
     name = row[:first_name]
     zipcode = clean_zipcode(row[:zipcode])
     phone = phone_number_validator(row[:homephone])
     regtime = get_registration_time(row[:regdate])
+    regtimes << regtime
     legislators = legislators_by_zipcode(zipcode)
     form_letter = erb_template.result(binding) #turns erb file into html, with the variable values as they exist in scope at this time
     save_thank_you_letter(id, form_letter)
-    puts regtime
+    # puts "NAME: #{name}", "ZIP: #{zipcode}", "PHONE: #{phone}", "REG_TIME: #{regtime}", " "
   end
+  most_common_regtimes = get_most_common_regtimes(regtimes)
+  puts most_common_regtimes
 end
 
 def save_thank_you_letter(id,form_letter)
