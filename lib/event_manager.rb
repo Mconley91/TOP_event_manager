e0e957f54ccd3983379c7b8148bb3e1a78519b25
mconley91 <matthew.e.conley@gmail.com>
2025-02-19 19:19:33 -0500

    added phone number validator

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,19 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,'0')[0..4]
 end
 
+def phone_number_validator(contents)
+  contents.each do |row|
+    phone = row[:homephone].split('').map {|char| Integer(char, exception: false) ? char : ''}.join('')
+    if phone.length == 11 && phone.split('')[0] == '1'
+      puts "#{row[:homephone][1..10]} is a good number."
+    elsif phone.length == 10
+      puts "#{row[:homephone]} is a good number."
+    else
+      puts "#{row[:homephone]} is a bad number."
+    end
+  end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = File.read('secretkey.txt').strip
@@ -31,6 +44,24 @@ def save_thank_you_letter(id,form_letter)
   end
 end
 
+def write_letters(contents)
+  template_letter = File.read('form_letter.erb')
+  erb_template = ERB.new template_letter
+
+  contents.each do |row|
+    id = row[0]
+    name = row[:first_name]
+
+    zipcode = clean_zipcode(row[:zipcode])
+
+    legislators = legislators_by_zipcode(zipcode)
+
+    form_letter = erb_template.result(binding)
+
+    save_thank_you_letter(id, form_letter)
+  end
+end
+
 puts 'Event Manager Initialized.'
 
 contents = CSV.open(
@@ -39,18 +70,5 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-template_letter = File.read('form_letter.erb')
-erb_template = ERB.new template_letter
-
-contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
-
-  zipcode = clean_zipcode(row[:zipcode])
-
-  legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letter(id, form_letter)
-end
+# write_letters(contents)
+phone_number_validator(contents)
